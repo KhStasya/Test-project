e8df4d09fac622dbd30e688d8743fa7c87b9f229
KhStasya <38671607+KhStasya@users.noreply.github.com>
2018-08-23 20:03:33 +0300

    Update Lesson3.js

--- Lesson3.js
+++ Lesson3.js
@@ -1,9 +1,8 @@
 /*Доработать проект MOOD так, чтобы цвета не повторялись. 
 Для контроля повторения цветов использовать хэш.
 */
-
-function randomDiap(m) {
-    return Math.floor(Math.random() * m ) + 1;
+function randomDiap(n,m) {
+    return Math.floor(Math.random()*(m-n+1))+n;
 }
 
 function mood(colorsCount) {
@@ -12,7 +11,7 @@ function mood(colorsCount) {
     console.log('цветов: ' + colorsCount);
 
     for (var i = 1; i <= colorsCount;) {
-        var n = randomDiap(colorsCount);
+        var n = randomDiap(1,7);
         var colorName = colors[n];
         if (colorName in used) // встречалось ли оно?
             continue;
